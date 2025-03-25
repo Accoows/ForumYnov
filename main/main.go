eca185a6da71c981aee71bc3a44c844813d02ff1
Fabien Ori <fabien.ori@ynov.com>
2025-03-25 12:30:52 +0100

    changement nom fichier mobile>index, Script>script, ajout register page aucun bouton

--- main/main.go
+++ main/main.go
@@ -28,11 +28,15 @@ func openBrowser(url string) {
 
 func main() {
 	// Parse the HTML template file
-	tmpl, err := template.ParseFiles(filepath.Join("./Templates/", "mobile.html"))
+	tmpl, err := template.ParseFiles(filepath.Join("./Templates/", "index.html"))
 	if err != nil {
 		panic(err)
 	}
 	tmplLogin, err := template.ParseFiles(filepath.Join("./Templates/", "login.html"))
+	if err != nil {
+		panic(err)
+	}
+	tmplregister, err := template.ParseFiles(filepath.Join("./Templates/", "register.html"))
 	if err != nil {
 		panic(err)
 	}
@@ -52,18 +56,27 @@ func main() {
 		}
 	})
 
+	//handler function to serve the register template
+	http.HandleFunc("/register.html", func(w http.ResponseWriter, r *http.Request) {
+		err := tmplregister.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	// Gère les requêtes vers le dossier "Scripts", de manière similaire au dossier "Styles".
-	http.Handle("/Scripts/", http.StripPrefix("/Scripts/", http.FileServer(http.Dir("./Scripts"))))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("./scripts"))))
 
 	// Serve static files (CSS, images, etc.) from the current directory
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
+	//serve static file from the Templates folder
 	http.Handle("/Templates/", http.StripPrefix("/Templates/", http.FileServer(http.Dir("./Templates"))))
 
 	// Ouvre automatiquement le navigateur web à l'adresse "http://localhost:8080"
 	fmt.Println("Starting server at port 8080")
 
-	const url = "http://localhost:8080"
+	//const url = "http://localhost:8080"
 	// Démarre le serveur HTTP sur le port 8080
 	go openBrowser("http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
